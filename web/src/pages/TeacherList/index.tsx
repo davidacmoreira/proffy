16f2b8418d15f69e316fa92f92a1e7761a590a5f
David Moreira <davidmoreira37@gmail.com>
2020-08-04 22:36:23 +0100

    Add page header

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
 
+import PageHeader from '../../components/PageHeader';
+
+import './styles.css';
+
 function TeacherList() {
   return (
-    <h1>Study</h1>
+    <div id="page-teacher-list" className="container">
+      <PageHeader title="Available Teachers">
+        <form id="search-teachers">
+          <div className="input-block">
+            <label htmlFor="subject">Subject</label>
+            <input id="subject" type="text" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="week_day">Day of Week</label>
+            <input id="week_day" type="text" />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="time">Hour</label>
+            <input id="time" type="text" />
+          </div>
+        </form>
+      </PageHeader>
+    </div>
   )
 }
 
