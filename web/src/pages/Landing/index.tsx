227efeaae1cce0bc1fbd7ec6e13e0762517738da
David Moreira <davidmoreira37@gmail.com>
2020-08-07 08:52:24 +0100

    Add show total connections in Landing page

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
@@ -11,6 +13,16 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
 
 function Landing() {
+  const [totalConnections, setTotalConnections] = useState(0);
+
+  useEffect(() => {
+    api.get('connections').then(response => {
+      const { total } = response.data;
+
+      setTotalConnections(total);
+    });
+  }, []);
+
   return (
     <div id="page-landing">
       <div id="page-landing-content" className="container">
@@ -38,7 +50,7 @@ function Landing() {
         </div>
 
         <span className="total-connections">
-          Total of xxx connections <img src={purpleHeartIcon} alt="Purple Heart" />
+          Total of { totalConnections } connections <img src={purpleHeartIcon} alt="Purple Heart" />
         </span>
       </div>
     </div>
